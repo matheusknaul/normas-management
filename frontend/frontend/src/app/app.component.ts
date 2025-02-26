d0e9aca1f3ba6b9c7307fe4eebee3326c1a6e70e
matheusknaul <contatomatheush0909@gmail.com>
2025-02-26 04:03:10 -0300

    update home page

--- frontend/frontend/src/app/app.component.ts
+++ frontend/frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from "./components/header/header.component";
+import { TapeComponent } from './components/tape/tape.component';
 import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent, HeaderComponent, FooterComponent],
+  imports: [HomeComponent, HeaderComponent, TapeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ frontend/frontend/src/app/components/tape/tape.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tape',
+  imports: [],
+  templateUrl: './tape.component.html',
+  styleUrl: './tape.component.css'
+})
+export class TapeComponent {
+
+}
